82c4aee15d7ac15a6d24c6948936e14675405e8e
thadoos <thaddeus.ong.23@ucl.ac.uk>
2025-11-18 18:00:03 +0000

    Added auth token to all api call

--- src/services/http.ts
+++ src/services/http.ts
@@ -5,4 +5,14 @@ const api = axios.create({
   baseURL: BASE_URL,
 });
 
+// Add this interceptor to inject the token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;

90b8046008b0ab942d5dea529005b535cbfd274c
Momo4811 <mohamedmohamed4881@gmail.com>
2026-02-12 23:27:50 +0000

    Hide discount in review modal when value is zero

--- src/pages/CateringOrdersTableView.tsx
+++ src/pages/CateringOrdersTableView.tsx
@@ -569,12 +569,14 @@ const CateringOrderDetailsModal = ({ order, isOpen, onClose, onOrderUpdated }: {
                                   }`}
                                 />
                               </div>
-                              <div>
-                                <p className="text-xs text-gray-500">Discount</p>
-                                <p className="font-semibold text-gray-900">
-                                  {formatCurrency(Number(session.promoDiscount || 0) + Number(session.promotionDiscount || 0))}
-                                </p>
-                              </div>
+                              {(Number(session.promoDiscount || 0) + Number(session.promotionDiscount || 0)) > 0 && (
+                                <div>
+                                  <p className="text-xs text-gray-500">Discount</p>
+                                  <p className="font-semibold text-gray-900">
+                                    {formatCurrency(Number(session.promoDiscount || 0) + Number(session.promotionDiscount || 0))}
+                                  </p>
+                                </div>
+                              )}
                             </div>
 
                             {/* Collection Time for this session */}

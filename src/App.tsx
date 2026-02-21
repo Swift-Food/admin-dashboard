d29336f30b884b5150cc4c685a015bcb73c1ce27
Arnav Vaish <zcabav0@ucl.ac.uk>
2026-02-21 17:08:16 +0000

    merge completed

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,8 @@ import StripeAccountsScreen from "./pages/StripeAccountsScreen";
 import MiscellaneousScreen from "./pages/MiscellaneousScreen";
 import EventCategoriesScreen from "./pages/EventCategoriesScreen/EventCategoriesScreen";
 import EventsScreen from "./pages/EventsScreen/EventsScreen";
-<<<<<<< HEAD
 import EventLocationsScreen from "./pages/EventLocationsScreen/EventLocationsScreen";
-=======
 import CalendarsScreen from "./pages/CalendarsScreen/CalendarsScreen";
->>>>>>> 47f7ff654f905126a1875e75fa63dc96b558e669
 import CateringSessionsScreen from "./pages/CateringSessionsScreen";
 import authService from "./services/auth.service";
 import LoginScreen from "./pages/LoginScreen";
@@ -53,11 +50,8 @@ export const pathToPageMap: Record<string, SidebarPage> = {
   miscellaneous: "miscellaneous",
   "event-categories": "event-categories",
   events: "events",
-<<<<<<< HEAD
   "event-locations": "event-locations",
-=======
   calendars: "calendars",
->>>>>>> 47f7ff654f905126a1875e75fa63dc96b558e669
   bundles: "bundles",
 };
 
@@ -79,11 +73,8 @@ export const pageToPathMap: Record<SidebarPage, string> = {
   miscellaneous: "miscellaneous",
   "event-categories": "event-categories",
   events: "events",
-<<<<<<< HEAD
   "event-locations": "event-locations",
-=======
   calendars: "calendars",
->>>>>>> 47f7ff654f905126a1875e75fa63dc96b558e669
   bundles: "bundles",
 };
 
@@ -109,11 +100,8 @@ const prismoPages: SidebarPage[] = [
   "home",
   "event-categories",
   "events",
-<<<<<<< HEAD
   "event-locations",
-=======
   "calendars",
->>>>>>> 47f7ff654f905126a1875e75fa63dc96b558e669
   "bundles",
 ];
 
@@ -178,13 +166,10 @@ function PageRenderer() {
         return <EventCategoriesScreen />;
       case "events":
         return <EventsScreen />;
-<<<<<<< HEAD
       case "event-locations":
         return <EventLocationsScreen />;
-=======
       case "calendars":
         return <CalendarsScreen />;
->>>>>>> 47f7ff654f905126a1875e75fa63dc96b558e669
       default:
         return <RestaurantAdminDashboard />;
     }
